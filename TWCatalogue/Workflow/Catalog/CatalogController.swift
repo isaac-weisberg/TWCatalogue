311818a4030f8ff423d78f4ebfa30835a746bf99
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-12-09 20:17:44 +0300

    rxflaws codesamples

--- TWCatalogue/Workflow/Catalog/CatalogController.swift
+++ TWCatalogue/Workflow/Catalog/CatalogController.swift
@@ -23,5 +23,20 @@ class CatalogController: UIViewController {
             .disposed(by: disposeBag)
 
         viewModel.reload.onNext(())
+
+        DispatchQueue(label: "start q", attributes: .concurrent).async {
+            let one = Observable.just(())
+                .observeOn(MainScheduler.instance)
+            let two = Observable.just(())
+                .delay(.milliseconds(40), scheduler: ConcurrentDispatchQueueScheduler.init(queue: .init(label: "asdf1", attributes: .concurrent)))
+
+            Observable
+                .combineLatest(one, two) { _, _ in () }
+                .subscribeOn(ConcurrentDispatchQueueScheduler(queue: .global()))
+                .subscribe(onNext: { void in
+                    print(void)
+                })
+                .disposed(by: self.disposeBag)
+        }
     }
 }
